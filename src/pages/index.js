b7b6e86227424c1538b11ba2a43f7fdce48dee94
Sumit Kumar <sumit.kumar@pinelabs.com>
2021-05-23 00:54:00 +0530

    amazon pro

---
+++ src/pages/index.js
@@ -0,0 +1,34 @@
+import Head from 'next/head';
+import Banner from '../components/Banner';
+import Header from '../components/Header';
+import ProductFeed from '../components/ProductFeed';
+
+export default function Home({ products }) {
+  return (
+    <div className='bg-gray-100'>
+      <Head>
+        <title>Amazon Pro</title>
+        <link rel="icon" href="/Amazon_icon.png" />
+      </Head>
+
+      <Header />
+
+      <main className='max-w-screen-2xl mx-auto'>
+        <Banner />
+        <ProductFeed products={products} />
+      </main>
+    </div>
+  );
+}
+
+export async function getServerSideProps(context) {
+  const products = await fetch('https://fakestoreapi.com/products').then(
+    (res) => res.json()
+  );
+
+  return {
+    props: {
+      products,
+    },
+  };
+}
