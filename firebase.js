5c5590df14dda78326e9564ce4a9fc9a0d7b7c2d
Sumit Kumar <sumit.kumar@pinelabs.com>
2021-05-23 14:37:07 +0530

    auth

---
+++ firebase.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase';
+const firebaseConfig = {
+  apiKey: 'AIzaSyDCHR_tjPqZnzdbeqDM_FY_HA0GPbp1nxM',
+  authDomain: 'pro-f174f.firebaseapp.com',
+  projectId: 'pro-f174f',
+  storageBucket: 'pro-f174f.appspot.com',
+  messagingSenderId: '722317989606',
+  appId: '1:722317989606:web:0a92fd1d4cc5551dad0d27',
+};
+
+
+
+
